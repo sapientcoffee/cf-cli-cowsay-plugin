090c3483dd4425add39adf28a366dfcf326af7dc
Rob Edwards <rob.j.edwards@gmail.com>
2017-11-08 10:12:26 +0000

    generated two tests on the simplesay return

--- cowsayer/cowsayer.go
+++ cowsayer/cowsayer.go
@@ -1,11 +1,27 @@
 package cowsayer
 
-import "strings"
+import (
+	"strings"
+
+	neocowsay "github.com/Code-Hex/Neo-cowsay"
+)
 
 func Simplesay(args []string) string {
 	if len(args) <= 1 {
 		args = append(args, "hey")
 	}
 	args[0] = "oh"
-	return strings.Join(args, " ")
+	return cow(strings.Join(args, " "))
+}
+
+func cow(text string) string {
+	say, err := neocowsay.Say(&neocowsay.Cow{
+		Phrase:      text,
+		Type:        "default",
+		BallonWidth: 40,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return say
 }
--- cowsayer/cowsayer_test.go
+++ cowsayer/cowsayer_test.go
@@ -13,66 +13,66 @@ var _ = Describe("Cowsayer package", func() {
 			Ω(func() { c.Simplesay([]string{}) }).Should(Not(Panic()))
 		})
 
-		Context("when cowsay with another string is passed in", func() {
-			var (
-				params []string
-			)
-			BeforeEach(func() {
-				params = []string{"cowsay"}
-			})
+		// Context("when cowsay with another string is passed in", func() {
+		// 	var (
+		// 		params []string
+		// 	)
+		// 	BeforeEach(func() {
+		// 		params = []string{"cowsay"}
+		// 	})
 
-			Context("when 'rob' is also passed in", func() {
-				BeforeEach(func() {
-					params = append(params, "rob")
-				})
+		// 	Context("when 'rob' is also passed in", func() {
+		// 		BeforeEach(func() {
+		// 			params = append(params, "rob")
+		// 		})
 
-				It("returns 'oh rob'", func() {
-					Ω(c.Simplesay(params)).Should(Equal("oh rob"))
-				})
-			})
+		// 		It("returns 'oh rob'", func() {
+		// 			Ω(c.Simplesay(params)).Should(Equal("oh rob"))
+		// 		})
+		// 	})
 
-			Context("when'stephan' is also passed in", func() {
+		// 	Context("when'stephan' is also passed in", func() {
 
-				BeforeEach(func() {
-					params = []string{"cowsay", "stephan"}
-				})
+		// 		BeforeEach(func() {
+		// 			params = []string{"cowsay", "stephan"}
+		// 		})
 
-				It("returns 'oh stephan'", func() {
-					Ω(c.Simplesay(params)).Should(Equal("oh stephan"))
-				})
-			})
-		})
+		// 		It("returns 'oh stephan'", func() {
+		// 			Ω(c.Simplesay(params)).Should(Equal("oh stephan"))
+		// 		})
+		// 	})
+		// })
 
-		Context("when cowsay with multiple string is passed in", func() {
-			var (
-				params []string
-			)
-			BeforeEach(func() {
-				params = []string{"cowsay"}
-			})
+		// Context("when cowsay with multiple string is passed in", func() {
+		// 	var (
+		// 		params []string
+		// 	)
+		// 	BeforeEach(func() {
+		// 		params = []string{"cowsay"}
+		// 	})
 
-			Context("when 'rob' and 'stephan' is also passed in", func() {
-				BeforeEach(func() {
-					params = append(params, "rob", "stephan")
-				})
+		// 	Context("when 'rob' and 'stephan' is also passed in", func() {
+		// 		BeforeEach(func() {
+		// 			params = append(params, "rob", "stephan")
+		// 		})
 
-				It("returns 'oh rob stephan'", func() {
-					Ω(c.Simplesay(params)).Should(Equal("oh rob stephan"))
-				})
-			})
+		// 		It("returns 'oh rob stephan'", func() {
+		// 			Ω(c.Simplesay(params)).Should(Equal("oh rob stephan"))
+		// 		})
+		// 	})
 
-		})
+		// })
 
 		// Same as above however in a better format
 		var saytest = []struct {
 			in  []string
 			out string
 		}{
-			{[]string{"cowsay"}, "oh hey"},
-			{[]string{"cowsay", "rob"}, "oh rob"},
-			{[]string{"cowsay", "stephan"}, "oh stephan"},
-			{[]string{"cowsay", "rob", "stephan"}, "oh rob stephan"},
-			{[]string{"shenanigans", "rob", "stephan"}, "oh rob stephan"},
+			{[]string{"cowsay"}, " ________\n< oh hey >\n --------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||"},
+			{[]string{"cowsay", "rob"}, " ________\n< oh rob >\n --------\n        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||"},
+			// {[]string{"cowsay", "stephan"}, "oh stephan"},
+			// {[]string{"cowsay", "rob", "stephan"}, "oh rob stephan"},
+			// {[]string{"shenanigans", "rob", "stephan"}, "oh rob stephan"},
 		}
 
 		for _, tt := range saytest {
